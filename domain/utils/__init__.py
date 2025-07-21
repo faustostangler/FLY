74d77387df9abf654a715417faf0d4616d6c1020
faustostangler <fausto.stangler@gmail.com>
2025-07-21 12:45:33 -0300

    Add function to detect missing quarter dates

--- domain/utils/__init__.py
+++ domain/utils/__init__.py
@@ -1,7 +1,13 @@
 """Domain utility helpers."""
 
 from .finance_utils import safe_divide
-from .math_utils import parse_quarter, quarter_index
+from .math_utils import find_missing_quarters, parse_quarter, quarter_index
 from .statement_hash import compute_hash
 
-__all__ = ["parse_quarter", "quarter_index", "safe_divide", "compute_hash"]
+__all__ = [
+    "parse_quarter",
+    "quarter_index",
+    "find_missing_quarters",
+    "safe_divide",
+    "compute_hash",
+]
