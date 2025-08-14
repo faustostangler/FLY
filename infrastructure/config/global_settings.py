0e03bc4585b58e7a4fc8c0ad672ac1b7cf569638
Fausto Stangler (Gamer) <fausto.stangler@gmail.com>
2025-08-13 23:19:48 -0300

    adjustments

--- infrastructure/config/global_settings.py
+++ infrastructure/config/global_settings.py
@@ -7,7 +7,7 @@ APP_NAME = "FLY" + "/" + VERSION  # Application name
 
 WAIT = 2  # Default wait time in seconds
 MAX_LINEAR_HOLES = 200  # Maximum number of linear holes allowed
-MAX_WORKERS = 60 # 20 Default number of threads for sync operations
+MAX_WORKERS = 10 # 20 Default number of threads for sync operations
 THRESHOLD = 10 # (MAX_WORKERS)  # max(int(50 / MAX_WORKERS), 1)  # Default threshold for saving data
 QUEUE_SIZE = 2 * MAX_WORKERS  # Max queue size for producer/consumer pipeline
 BATCH_SIZE = 100  # Number of items per repository batch
