4551c1fd00c99e85a97b628f7d3afa0c79372ab0
faustostangler <fausto.stangler@gmail.com>
2025-11-02 17:37:49 -0300

    Remove statements ratio model and related code

--- domain/ports/repository_indicators_port.py
+++ domain/ports/repository_indicators_port.py
@@ -1,19 +1,11 @@
 from __future__ import annotations
 
-from typing import List, Protocol, runtime_checkable
+from typing import Protocol, runtime_checkable
 
-from application.ports.uow_port import Uow
-
-# from application.ports.uow_port import Uow
 from domain.dtos.indicators_dto import IndicatorsDTO
-from domain.dtos.statement_ratio_dto import StatementRatioDTO
 from domain.ports.repository_base_port import RepositoryBasePort
 
 
 @runtime_checkable
 class RepositoryIndicatorsPort(RepositoryBasePort[IndicatorsDTO, int], Protocol):
-    """
-    """
-
-    def save_ratios_batch(self, ratios: List[StatementRatioDTO], *, uow: Uow) -> None:
-        """Persist ratios already normalized to long format."""
+    """Port definition for repositories handling indicator data."""
