8850e3ed85ff98f7fc876fdbab0d4212115b0d1f
Fausto Stangler <fausto.stangler@gmail.com>
2025-11-08 08:03:59 -0300

    adjustments

--- presentation/frontend/src/config/companyFacets.js
+++ presentation/frontend/src/config/companyFacets.js
@@ -3,9 +3,9 @@ export const COMPANY_FACETS = [
   { field: 'subsector', label: 'Subsetor', multiple: true },
   { field: 'segment', label: 'Segmento', multiple: true },
   { field: 'market', label: 'Mercado', multiple: true },
-  { field: 'company_name', label: 'Companhia', multiple: true }, // false to radio
-  { field: 'trading_name', label: 'Nome de pregão', multiple: true },
-  { field: 'ticker', label: 'Ticker', multiple: true },
-  { field: 'institution_common', label: 'Instituição Ordinária', multiple: true },
-  { field: 'institution_preferred', label: 'Instituição Preferencial', multiple: true },
+  // { field: 'company_name', label: 'Companhia', multiple: true }, // false to radio
+  // { field: 'trading_name', label: 'Nome de pregão', multiple: true },
+  // { field: 'ticker', label: 'Ticker', multiple: true },
+  // { field: 'institution_common', label: 'Instituição Ordinária', multiple: true },
+  // { field: 'institution_preferred', label: 'Instituição Preferencial', multiple: true },
 ]
